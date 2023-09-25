6f501d1d8d03ca9e2af884ca27108f95e107e19b
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-09-25 15:16:41 -0700

    fix tweeks

--- src/lib/components/CheckoutForm.svelte
+++ src/lib/components/CheckoutForm.svelte
@@ -64,8 +64,8 @@
           id="message"
           bind:value={message}
           rows="4"
-          required
-          class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-royalBlue-500"
+          placeholder="KDD에 하시고 싶으신 말씀이나 건의사항이 있다면 남겨주세요."
+          class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-royalBlue-500 text-sm"
         ></textarea>
       </div>
 
