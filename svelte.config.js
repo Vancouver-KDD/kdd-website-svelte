dc2685bd00c5a5a23a37b17f8c5c6e259c153a07
Eric Kim <bosung90@gmail.com>
2023-10-06 14:02:02 -0700

    fix csrf

--- svelte.config.js
+++ svelte.config.js
@@ -31,9 +31,9 @@ const config = {
         // allowQuery: ['search'],
       },
     }),
-  },
-  csrf: {
-    checkOrigin: false,
+    csrf: {
+      checkOrigin: false,
+    },
   },
   vitePlugin: {
     inspector: true,
