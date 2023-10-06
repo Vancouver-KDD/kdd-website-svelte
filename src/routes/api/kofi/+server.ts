f267081f1c5e45330dda6b11aff9b665d07adcb3
Eric Kim <bosung90@gmail.com>
2023-10-06 14:27:40 -0700

    try logging out

--- src/routes/api/kofi/+server.ts
+++ src/routes/api/kofi/+server.ts
@@ -3,11 +3,15 @@ import {json} from '@sveltejs/kit'
 
 export async function POST({request}) {
   const formData = await request.formData()
+  console.log({formData})
   const webhookJsonString = formData.get('data')?.toString()
+  console.log({webhookJsonString})
   if (!webhookJsonString) {
     throw new Error('Invalid formData, missing "data"')
   }
   const webhookJson = JSON.parse(webhookJsonString) as App.KoFiWebhookData
+  console.log({webhookJson})
+
   await handleKofiWebhook(webhookJson)
   return json({status: 'success'})
 }
