9a96e54576265541d6d451dacd035390a66ed3d0
Eric Kim <bosung90@gmail.com>
2023-10-06 14:12:16 -0700

    fix parsing kofi data

--- src/routes/api/kofi/+server.ts
+++ src/routes/api/kofi/+server.ts
@@ -3,8 +3,11 @@ import {json} from '@sveltejs/kit'
 
 export async function POST({request}) {
   const formData = await request.formData()
-  console.log({formData})
-  const webhookJson = Object.fromEntries(formData.entries()) as unknown as App.KoFiWebhookData
+  const webhookJsonString = formData.get('data')?.toString()
+  if (!webhookJsonString) {
+    throw new Error('Invalid formData, missing "data"')
+  }
+  const webhookJson = JSON.parse(webhookJsonString) as App.KoFiWebhookData
   await handleKofiWebhook(webhookJson)
   return json({status: 'success'})
 }
