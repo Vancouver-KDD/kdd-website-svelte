7b8e93c618b86af88f5ff490befca918dc8e4de9
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-10-05 17:54:05 -0700

    Feat: create kdd admin pages & implememt firebase auth

---
+++ src/routes/admin/success/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let email: string
+  let password: string
+</script>
+
+<svelte:head>
+  <title>KDD Admin Page</title>
+</svelte:head>
+
+<section class="flex flex-col items-center gap-4 p-10">
+  <h1 class="text-xl font-semibold">Hello World!</h1>
+</section>
