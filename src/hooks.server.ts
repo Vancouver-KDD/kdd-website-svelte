1a8b87adcaab54838df8d78489b31b315229e3c2
Eric Kim <bosung90@gmail.com>
2023-10-14 00:35:44 -0700

    increase bypasstoken size to 40 chars

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,10 @@ import type {Handle} from '@sveltejs/kit'
 export const handle: Handle = async ({event, resolve}) => {
   console.log('event.request.method', event.request.method)
   if (event.request.method == 'POST') {
-    event.request.headers.append('x-prerender-revalidate', '0VkJCrieFXnOIRGqLdqf')
+    event.request.headers.append(
+      'x-prerender-revalidate',
+      '0VkJCrieFXnOIRGqLdqf0VkJCrieFXnOIRGqLdqf'
+    )
   }
   const response = await resolve(event)
   return response
